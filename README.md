11e444e7114212c0d0a4adcb14fe20dda555b9b3
Bartosz Drozd <bartosz.drozd94@gmail.com>
2021-05-05 01:21:36 +0200

    changes

---
+++ README.md
@@ -0,0 +1,13 @@
+# flask-static-potfolio
+<<<<<<< Updated upstream
+Hello, this is an aplha verion of my static website made using these technologies:
+- Flask framework
+- Frozen-flask -> module used to generate static files based on a Flask app
+- elsa -> Helper module for hosting Frozen-Flask based websites on GitHub pages (https://github.com/pyvec/elsa)
+=======
+Hello, this is an alpha version of my static website made using these technologies:
+- Flask framework
+- Frozen-flask -> module used to generate static files based on a Flask app
+- elsa -> Helper module for hosting Frozen-Flask based websites on GitHub pages (https://github.com/pyvec/elsa)
+>>>>>>> origin/master
+>>>>>>> Stashed changes
---
+++ sitebuilder.py
@@ -0,0 +1,63 @@
+import sys
+import os
+#import elsa
+
+from flask import Flask, render_template, send_from_directory, redirect
+from flask_frozen import Freezer
+from flask_fontawesome import FontAwesome
+
+FONTAWESOME_SERVE_LOCAL = True 
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+freezer = Freezer(app)
+fa = FontAwesome(app)
+name="Bartosz Drozd"
+
+FREEZER_DESTINATION = 'docs'
+app.config['FONTAWESOME_STYLES'] = ['brands']
+
+"""@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')"""
+
+@app.route('/')
+def home():
+    return render_template('about.html')
+
+@app.route('/about/')
+def index():
+    return render_template('about.html')
+
+@app.route('/my-projects/')
+def portfolio():
+    return render_template('my-projects.html')
+
+@app.route('/my-resume/')
+def resume():
+    return render_template('my-resume.html')
+
+@app.route('/my-trips/')
+def trips():
+    return render_template('my-trips.html')
+
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
+#using elsa to deploy this shit 
+"""if __name__ == '__main__':
+    from elsa import cli
+    cli(app, base_url='https://bartoszdrozd.github.io')"""
+
+if len(sys.argv) > 1 and sys.argv[1] == "build":
+    freezer.freeze()
+else:
+    app.run() # for development (debug=True, port=8000)
+        

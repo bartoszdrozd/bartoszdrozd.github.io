b298837623536b511ea2dfadcf95a8511cd9684d
Bartosz Drozd <bartosz.drozd94@gmail.com>
2021-05-31 22:32:33 +0200

    new error templates

--- sitebuilder.py
+++ sitebuilder.py
@@ -48,9 +48,13 @@ def contact():
     return render_template('contact.html')
 
 @app.errorhandler(404)
-def page_not_found(error):
-    return render_template('page_not_found.html'), 404
+def not_found_error(error):
+    return render_template('404.html'), 404
 
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+    
 #using elsa to deploy this shit 
 """if __name__ == '__main__':
     from elsa import cli

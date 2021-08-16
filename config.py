33cfec48a5a3ca412b17e321cf9c1c5d3bb576c4
Bartosz Drozd <bartosz.drozd94@gmail.com>
2021-08-16 23:58:07 +0200

    restructure

--- config.py
+++ config.py
@@ -6,14 +6,14 @@ load_dotenv(dotenv_path)
 
 class Config(object):
 	MAPBOX_ACCESS_KEY = os.environ.get('MAPBOX_ACCESS_KEY')
-	app.config.from_envvar('APP_CONFIG_FILE', silent=True)
-	app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-	app.config['WTF_CSRF_SECRET_KEY'] = os.getenv('SECRET_KEY')
-	app.config['MAIL_SERVER']='smtp.gmail.com'
-	app.config['MAIL_PORT']=465
-	app.config['MAIL_USE_SSL']=1
-	app.config["MAIL_USERNAME"] = os.getenv('MAIL_USERNAME')
-	app.config["MAIL_PASSWORD"] = os.getenv('MAIL_PASSWORD')
-	app.config['FONTAWESOME_STYLES'] = ['all']
+	#app.config.from_envvar('APP_CONFIG_FILE', silent=True)
+	SECRET_KEY = os.environ.get('SECRET_KEY')
+	WTF_CSRF_SECRET_KEY = os.getenv('SECRET_KEY')
+	MAIL_SERVER = 'smtp.gmail.com'
+	MAIL_PORT = 465
+	MAIL_USE_SSL = 1
+	MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+	MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+	FONTAWESOME_STYLES = ['all']
 	FONTAWESOME_SERVE_LOCAL = True 
 	name = "Bartosz Drozd"
